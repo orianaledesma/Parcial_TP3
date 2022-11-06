2cbb861822814eb5d82aaea6379828b3b17b46f1
AgusLois <Agusslois98@gmail.com>
2022-11-06 19:15:50 -0300

    Update CharacterHolder.kt

--- app/src/main/java/edu/ar/parcial_tp3/Holders/CharacterHolder.kt
+++ app/src/main/java/edu/ar/parcial_tp3/Holders/CharacterHolder.kt
@@ -1,4 +1,32 @@
 package edu.ar.parcial_tp3.Holders
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import edu.ar.parcial_tp3.R
 
-class CharacterHolder {
-}
+
+    class CharacterHolder (v: View) : RecyclerView.ViewHolder(v) {
+
+        private var view: View
+
+        init {
+            this.view = v
+        }
+
+        fun setName(name: String) {
+            val txt: TextView = view.findViewById(R.id.txt_name_card)
+            txt.text = name
+        }
+
+        fun setStatus(status: String) {
+            val txt: TextView = view.findViewById(R.id.txt_status)
+            txt.text = status
+        }
+
+        fun getCardLayout(): CardView {
+            return view.findViewById(R.id.txt_name_card)
+        }
+
+    }
