c9a852441b638b959915b75c6e57f59dd5b9cd51
AgusLois <Agusslois98@gmail.com>
2022-11-06 19:15:34 -0300

    Update ListAdapter.kt

--- app/src/main/java/edu/ar/parcial_tp3/Adapters/ListAdapter.kt
+++ app/src/main/java/edu/ar/parcial_tp3/Adapters/ListAdapter.kt
@@ -1,4 +1,30 @@
 package edu.ar.parcial_tp3.Adapters
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.text.TextUtils
+import androidx.recyclerview.widget.RecyclerView
+import edu.ar.parcial_tp3.Holders.CharacterHolder
+import edu.ar.parcial_tp3.R
+import edu.ar.parcial_tp3.Clases.Character
 
-class listAdapter {
-}
+
+
+class ListAdapter (
+        private val characterList: MutableList<Character>
+    ) : RecyclerView.Adapter<CharacterHolder>() {
+
+        override fun getItemCount() = characterList.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterHolder {
+            val view =  LayoutInflater.from(parent.context).inflate(R.layout.item_card,parent,false)
+            return (CharacterHolder(view))
+        }
+
+        override fun onBindViewHolder(holder: CharacterHolder, position: Int) {
+            val character = characterList[position]
+            holder.setName(TextUtils.concat(character.name, " (", character.status, ")").toString())
+            holder.setStatus(character.status)
+
+            }
+        }
+
